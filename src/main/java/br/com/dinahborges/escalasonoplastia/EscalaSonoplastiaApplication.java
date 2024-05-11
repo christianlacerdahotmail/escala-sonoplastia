951f214dfe08b86145a2d701096bba7719367a2c
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-05-11 19:53:44 -0300

    inicia o projeto

---
+++ src/main/java/br/com/dinahborges/escalasonoplastia/EscalaSonoplastiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dinahborges.escalasonoplastia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaSonoplastiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaSonoplastiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dinahborges/escalasonoplastia/EscalaSonoplastiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dinahborges.escalasonoplastia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaSonoplastiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
