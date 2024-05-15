a80511759bf678fc59b12c03f342b4b2bbefdbe6
christianlacerdahotmail <christianlacerda@hotmail.com>
2024-05-14 22:23:24 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/dinahborges/escalasonoplastia/operador/application/api/OperadorController.java
+++ src/main/java/br/com/dinahborges/escalasonoplastia/operador/application/api/OperadorController.java
@@ -2,15 +2,20 @@ package br.com.dinahborges.escalasonoplastia.operador.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.dinahborges.escalasonoplastia.operador.application.service.OperadorService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class OperadorController implements OperadorAPI {
+	private final OperadorService operadorService;
 
 	@Override
 	public OperadorResponse postOperador(OperadorRequest operadorRequest) {
 		log.info("[inicia] OperadorController - postOperdor");
+		OperadorResponse operadorCriado = operadorService.criaOperador(operadorRequest);
 		log.info("[finaliza] OperadorController - postOperdor");
 		return null;
 	}
---
+++ src/main/java/br/com/dinahborges/escalasonoplastia/operador/application/service/OperadorService.java
@@ -0,0 +1,9 @@
+package br.com.dinahborges.escalasonoplastia.operador.application.service;
+
+import br.com.dinahborges.escalasonoplastia.operador.application.api.OperadorRequest;
+import br.com.dinahborges.escalasonoplastia.operador.application.api.OperadorResponse;
+
+public interface OperadorService {
+	OperadorResponse criaOperador(OperadorRequest operadorRequest);
+
+}
